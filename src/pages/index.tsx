587f3a5b361b0a39e6ca998b12b32e4f76771099
Emmanuel Telewik <emmanuel.telewik@threetwentyfive.com.au>
2023-03-25 16:16:05 +0800

    added codemirror and c lang support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react"
+// import CodeMirror from '@uiw/react-codemirror';
+// import {StreamLanguage} from '@codemirror/language';
+// import { c } from '@codemirror/legacy-modes/mode/clike';
+// import { keymap } from "@codemirror/view";
+// import {insertTab, indentLess} from '@codemirror/commands'
 
 const Home: FC = () => {
   return (
-    <div className="mt-20">Home</div>
+    <div >
+
+    </div>
   )
 }
 
